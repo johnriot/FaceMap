fd91650d0b0424aa50d2f1bd5cc2423503f17c30
John O'Halloran <ohalloran.john@gmail.com>
2014-06-10 15:10:28 -0300

    Updated addon to cycle through all created files, add them to media library and create Question and Answer cards in Anki deck

--- addon/test.py
+++ addon/test.py
@@ -8,6 +8,9 @@ from anki.importing import TextImporter
 
 from PyQt4 import QtCore, QtGui
 
+from os import *
+from os.path import *
+
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
 
@@ -23,22 +26,52 @@ def testFunction():
     deck['mid'] = m['id']
     mw.col.decks.save(deck)    
     
-    # Add the image file to media
-    uniFilename = unicode("C:/Users/John/Pictures/arnie.jpg")
-    mw.col.media.addFile(uniFilename)
+    # Add each created image to the media folder
+    # TODO: Must change to os.path for Unix compatibility
+    uniDirName = unicode("C:/Users/John/workspace/PythonScripts/FaceMap/images")
+    listFiles = os.listdir(uniDirName)
+    for fileName in listFiles:
+        filePath = unicode(os.path.join(uniDirName, fileName))
+        mw.col.media.addFile(filePath)
     
-    # Create and add the note
-    note = mw.col.newNote()
-    note['Front'] = u'3'
-    note['Back'] = u"<img src='arnie.jpg'>" 
-    newCount = mw.col.addNote(note)
-    note.flush()
     
-    # show the new count
-    showInfo("John Card count: %d" % newCount)
+    # Find an image map
+    for i in range(0, len(listFiles) / 4):
+        qWindow, qPerson, aPerson, aWindow = findNoteTuple(listFiles, i)
+    
+    
+        # Create and add the note
+        note = mw.col.newNote()
+        print "qWindow: " + qWindow
+        print "aPerson: " + aPerson
+        note['Front'] = u"<img src='" + qWindow + "'>"
+        note['Front'] += u"<img src='" + qPerson + "'>"
+        note['Back'] = u"<img src='" + aPerson + "'>"
+        note['Back'] += u"<img src='" + aWindow + "'>"
+        newCount = mw.col.addNote(note)
+        note.flush()
     
     # Reset the GUI to show updated card counts etc
     mw.reset()
+
+# Find a tuple for the new note to be added
+def findNoteTuple(listFiles, indx):
+    for fn in listFiles:
+        fileString = str(fn)
+        
+        if(fileString.startswith('cwQ' + str(indx))):
+            qWindow = fn
+        elif(fileString.startswith('dfQ' + str(indx))):
+            qPerson = fn
+        elif(fileString.startswith('dfA' + str(indx))):
+            aPerson = fn
+        elif(fileString.startswith('cwA' + str(indx))):
+            aWindow = fn
+        
+            
+    # Make a tuple for the new note
+    #showInfo("noteTuple: %s, %s, %s, %s"  % (qWindow, qPerson, aPerson, aWindow))
+    return qWindow, qPerson, aPerson, aWindow
     
 
 # create a new menu item, "test"
